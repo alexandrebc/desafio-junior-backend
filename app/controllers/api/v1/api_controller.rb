6497a30c99fb986a5e30b1deaa631fc61bacf085
AlexandreB Costa <alexandrebc00@gmail.com>
2020-06-26 23:13:19 -0300

    file base to handle with auth in api

---
+++ app/controllers/api/v1/api_controller.rb
@@ -0,0 +1,26 @@
+# functions to handle auth by token in api
+class Api::V1::ApiController < ActionController::Base
+	before_action :require_login!
+	helper_method :person_signed_in?, :current_user
+
+	def user_signed_in?
+		current_person.present?
+  	end
+
+  	def require_login!
+  		return true if authenticate_token
+  		render json: { data: 'Acesso negado' }, status: 401
+  	end
+
+  	def current_user
+  		@_current_user ||= authenticate_token
+  	end
+
+  	private
+  		def authenticate_token
+  			authenticate_with_http_token do |token, options|
+  				User.find_by(token: token)
+  			end
+  		end
+
+end
