0410475fecd9018dd0014fcf20e79b4ab504fa49
Alexandre Barroso <alexandrebc00@gmail.com>
2020-06-28 15:17:32 -0300

    changed user_params to accept array and check pwd

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -11,11 +11,18 @@ module Api
 			# update user info
 			def update
 				# dont update password if both fields are not filled
-				if user_params[:password].blank? || user_params[:password_confirmation].blank? 
+				user_update = user_params
+				if user_params[:password].blank? || user_params[:password_confirmation].blank? || user_params[:password] != user_params[:password_confirmation]
+					if user_params[:password] != user_params[:password_confirmation]
+						pwdFlag = true
+					end
+
 					user_update = user_params.except(:password, :password_confirmation)
 				end
 
-				if current_user.update(user_update)
+				if pwdFlag
+					render json: { data: 'Revise se a senha foi corretamente digitada duas vezes.'}, status: :unprocessable_entity
+				elsif current_user.update(user_update)
 					render json: { data: 'Perfil editado com sucesso.' }, status: :ok
 				else
 					render json: { data: message.errors }, status: :unprocessable_entity
@@ -24,7 +31,7 @@ module Api
 
 			private
 				def user_params
-					params.permit(
+					params.require(:user).permit(
 						:name,
 						:email,
 						:password,
